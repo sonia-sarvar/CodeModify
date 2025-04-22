9a7f036f81d394cea2f30ac32e5cce85a484867e
sonia-sarvar <40298696+sonia-sarvar@users.noreply.github.com>
2025-04-23 01:56:27 +0530

    Update service.spec.ts

--- service.spec.ts
+++ service.spec.ts
@@ -5,6 +5,9 @@ import com.ptc.mvc.components.*;
 import com.ptc.netmarkets.util.beans.NmCommandBean;
 import com.ptc.netmarkets.util.beans.NmHelperBean;
 import static com.ptc.core.components.descriptor.DescriptorConstants.ColumnIdentifiers.ICON;
+import ext.cummins.part.CumminsPartConstantIF;
+import ext.cummins.utils.CumminsUtils;
+import wt.associativity.WTAssociativityHelper;
 import wt.fc.Persistable;
 import wt.fc.QueryResult;
 import wt.fc.WTObject;
@@ -14,22 +17,15 @@ import wt.part.WTPart;
 import wt.util.WTException;
 import wt.vc.VersionControlHelper;
 import wt.vc.Versioned;
-import wt.workflow.forum.DiscussionForum;
-import wt.workflow.forum.DiscussionTopic;
-import wt.workflow.forum.ForumHelper;
-
+import wt.vc.config.LatestConfigSpec;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 
 @ComponentBuilder("ext.cummins.part.mvc.builders.TestDiscussionTable")
 public class TestDiscussionTable extends AbstractComponentBuilder {
     private static final String CLASSNAME = TestDiscussionTable.class.getName();
     private static final Logger LOGGER = LogManager.getLogger(CLASSNAME);
     private static final String TYPE = "Type";
-    private static final String VERSION = "Version";
     private static final String TOPICS = "Topics/Comments";
     private static final String COMMENTS = "Comments";
     private static final String VERSION_VIEW_DISPLAY_NAME = "Version";
@@ -52,17 +48,17 @@ public class TestDiscussionTable extends AbstractComponentBuilder {
         col1.setLabel(TYPE);
         table.addComponent(col1);
 
-        ColumnConfig col2 = factory.newColumnConfig(VERSION, true);
-        col2.setLabel(VERSION);
+        ColumnConfig col2 = factory.newColumnConfig(TOPICS, false);
+        col2.setLabel(TOPICS);
         table.addComponent(col2);
 
-        ColumnConfig col3 = factory.newColumnConfig(TOPICS, true);
-        col3.setLabel(TOPICS);
-        table.addComponent(col3);
-
-        ColumnConfig col4 = factory.newColumnConfig(COMMENTS, true);
-        col4.setLabel(COMMENTS);
+        ColumnConfig col4 = factory.newColumnConfig("versionInfo.identifier.versionId", true);
+        col4.setLabel(VERSION_VIEW_DISPLAY_NAME);
         table.addComponent(col4);
+
+        ColumnConfig col5 = factory.newColumnConfig(COMMENTS, true);
+        col5.setLabel(COMMENTS);
+        table.addComponent(col5);
 
         LOGGER.debug("End >> TestDiscussionTable");
         return table;
@@ -76,12 +72,11 @@ public class TestDiscussionTable extends AbstractComponentBuilder {
         // Get the primary object (WTPart)
         Persistable requestObj = nmCommandBean.getPrimaryOid().getWtRef().getObject();
         WTPart wtpart = null;
-        ArrayList<Map<String, String>> discussionData = new ArrayList<>();
+        ArrayList<WTPart> listobj = new ArrayList<>();
 
         // Process only if the request object is a WTPart
         if (requestObj instanceof WTPart) {
             wtpart = (WTPart) requestObj;
-
             LOGGER.debug("Request object is a WTPart: " + wtpart.getDisplayIdentifier());
 
             // Fetch all versions of the WTPart using VersionControlHelper
@@ -99,38 +94,7 @@ public class TestDiscussionTable extends AbstractComponentBuilder {
                 Versioned versioned = (Versioned) versionQuery.nextElement();
                 if (versioned instanceof WTPart) {
                     WTPart versionPart = (WTPart) versioned;
-
-                    // Correct way to get version info (version ID)
-                    String versionId = versionPart.getVersionInfo().getVersionId();  // Get version ID from VersionInfo
-                    LOGGER.debug("Version ID: " + versionId);
-
-                    // Fetch discussion forums related to this part version
-                    Enumeration forums = ForumHelper.service.getForums(versionPart);
-
-                    // For each forum, fetch topics and messages
-                    while (forums.hasMoreElements()) {
-                        DiscussionForum forum = (DiscussionForum) forums.nextElement();
-                        Enumeration topics = ForumHelper.service.getTopics(forum);
-
-                        // Iterate through each topic
-                        while (topics.hasMoreElements()) {
-                            DiscussionTopic topic = (DiscussionTopic) topics.nextElement();
-                            Enumeration messages = ForumHelper.service.getPostings(topic);
-
-                            // Iterate through the messages in the topic
-                            while (messages.hasMoreElements()) {
-                                // Fetch the discussion message (you can retrieve more details here if needed)
-                                Map<String, String> rowData = new HashMap<>();
-                                rowData.put(VERSION_VIEW_DISPLAY_NAME, versionId); // Adding version ID
-                                rowData.put(TOPICS, topic.getName());  // Topic name
-                                rowData.put(COMMENTS, "Discussion content for " + topic.getName());  // Mockup content, replace with actual message content
-
-                                // Add the data to the list for each version/topic combination
-                                discussionData.add(rowData);
-                                LOGGER.debug("Fetched discussion: " + rowData);
-                            }
-                        }
-                    }
+                    listobj.add(versionPart);
                     LOGGER.debug("Fetched version: " + versionPart.getDisplayIdentifier());
                 }
             }
@@ -138,10 +102,10 @@ public class TestDiscussionTable extends AbstractComponentBuilder {
             LOGGER.debug("Request object is not a WTPart.");
         }
 
-        // Log the final number of discussions fetched
-        LOGGER.debug("Total discussions fetched: " + discussionData.size());
+        // Log the final number of parts fetched
+        LOGGER.debug("Total versions fetched: " + listobj.size());
 
-        // Return the discussion data for the table
-        return discussionData;
+        // Return the list of versions as the data for the table
+        return listobj;
     }
 }
